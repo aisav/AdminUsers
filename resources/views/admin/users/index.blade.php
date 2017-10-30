fd9f12682378ccf20b87d45b16503a123e18d241
arturisav <artur.isaverdyan@gmail.com>
2017-10-30 13:39:19 +0400

    users list

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,7 +1,36 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 29/10/2017
- * Time: 09:38
- */
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Users</h1>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Active</th>
+            <th>Created</th>
+            <th>Uptaded</th>
+        </tr>
+        </thead>
+        <tbody>
+        @if($users)
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->role->name}}</td>
+                    <td>{{$user->is_active == 1 ? 'Active' : 'Not active'}}</td>
+                    <td>{{$user->created_at}}</td>
+                    <td>{{$user->updated_at}}</td>
+                </tr>
+            @endforeach
+        @endif
+        </tbody>
+    </table>
+@endsection
+
+
+
