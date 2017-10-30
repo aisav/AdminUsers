800c990bfc5c4e9880a173591d3e01e5503b6125
arturisav <artur.isaverdyan@gmail.com>
2017-10-30 17:33:38 +0400

    laravel form with config

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,7 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 29/10/2017
- * Time: 15:37
- */
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Create User</h1>
+    {{--<form action="AdminUserController@store" method="get" target="_blank">--}}
+    {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
+    <div class="form-group">
+        {!! Form::label('name', 'Name') !!}
+        {!! Form::text('name', null,
+            ['class'=>'form-control', 'placeholder'=>'Name']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('email', 'Email') !!}
+        {!! Form::text('email', null,
+            ['class'=>'form-control', 'placeholder'=>'Email']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role') !!}
+        {!! Form::text('role_id', null,
+            ['class'=>'form-control', 'placeholder'=>'Role_id']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('status', 'Status') !!}
+        {!! Form::text('status', null,
+            ['class'=>'form-control', 'placeholder'=>'Status']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+    </div>
+    {!! Form::close() !!}
+    {{--</form>--}}
+@endsection
