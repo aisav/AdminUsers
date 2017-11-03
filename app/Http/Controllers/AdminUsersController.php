8b7bb0a5ffb87a12f161af934e9d19a663f4768a
arturisav <artur.isaverdyan@gmail.com>
2017-11-04 00:03:25 +0400

    edit user backend

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -94,9 +94,23 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersRequest $request, $id)
     {
         //
+        $user = User::findorFail($id);
+        $input = $request->all();
+        if ($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['path'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+        $input['password'] = bcrypt($request->password);
+        $user->update($input);
+
+//        User::create($request->all());
+        return redirect('/admin/users');
+
     }
 
     /**
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -3,60 +3,65 @@
 @section('content')
 
     <h1>Edit User</h1>
-    <div class="col-sm-3">
-        <img src="/images/{{$user->photo? $user->photo->path : 'http://placehold.it/400x400'}}" alt=""
-             class="img-responsive img-rounded">
+
+    <div class="row">
+        <div class="col-sm-3">
+            <img src="/images/{{$user->photo? $user->photo->path : 'http://placehold.it/400x400'}}" alt=""
+                 class="img-responsive img-rounded">
+        </div>
+        <div class="col-sm-9">
+            {{--<form action="AdminUserController@store" method="get" target="_blank">--}}
+            {!! Form::model($user, ['method'=>'PATCH','action'=>['AdminUsersController@update', $user->id], 'files'=>true]) !!}
+            <div class="form-group">
+                {!! Form::label('name', 'Name') !!}
+                {!! Form::text('name', null,
+                    ['class'=>'form-control', 'placeholder'=>'Name']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('email', 'Email') !!}
+                {!! Form::email('email', null,
+                    ['class'=>'form-control', 'placeholder'=>'Email']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('role_id', 'Role') !!}
+                {!! Form::select('role_id',[''=>'Chose Options'] + $roles, null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('is_active', 'Status') !!}
+                {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'), null,  ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('photo_id', 'Photo') !!}
+                {!! Form::file('photo_id', null,  ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('password', 'Password') !!}
+                {!! Form::password('password', ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+            </div>
+            {!! Form::close() !!}
+            {{--</form>--}}
+        </div>
     </div>
-    <div class="col-sm-9">
-        {{--<form action="AdminUserController@store" method="get" target="_blank">--}}
-        {!! Form::model($user, ['method'=>'PATCH','action'=>['AdminUsersController@update', $user->id], 'files'=>true]) !!}
-        <div class="form-group">
-            {!! Form::label('name', 'Name') !!}
-            {!! Form::text('name', null,
-                ['class'=>'form-control', 'placeholder'=>'Name']) !!}
-        </div>
 
-        <div class="form-group">
-            {!! Form::label('email', 'Email') !!}
-            {!! Form::email('email', null,
-                ['class'=>'form-control', 'placeholder'=>'Email']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('role_id', 'Role') !!}
-            {!! Form::select('role_id',[''=>'Chose Options'] + $roles, null, ['class'=>'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('is_active', 'Status') !!}
-            {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'), null,  ['class'=>'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('photo_id', 'Photo') !!}
-            {!! Form::file('photo_id', null,  ['class'=>'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('password', 'Password') !!}
-            {!! Form::password('password', ['class'=>'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
-        </div>
-        {!! Form::close() !!}
-        {{--</form>--}}
+    <div class="row">
+        @if(count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div>
-
-    @if(count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{$error}}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 
 @endsection
