f05211763f15766ec7f3ae3b880e136254ddf34f
arturisav <artur.isaverdyan@gmail.com>
2017-11-03 12:09:47 +0400

    edit user page

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,7 +1,56 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 29/10/2017
- * Time: 15:37
- */
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Edit User</h1>
+    {{--<form action="AdminUserController@store" method="get" target="_blank">--}}
+    {!! Form::model($user, ['method'=>'PATCH','action'=>['AdminUsersController@update', $user->id], 'files'=>true]) !!}
+    <div class="form-group">
+        {!! Form::label('name', 'Name') !!}
+        {!! Form::text('name', null,
+            ['class'=>'form-control', 'placeholder'=>'Name']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('email', 'Email') !!}
+        {!! Form::email('email', null,
+            ['class'=>'form-control', 'placeholder'=>'Email']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role') !!}
+        {!! Form::select('role_id',[''=>'Chose Options'] + $roles, null, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('is_active', 'Status') !!}
+        {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'), null,  ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('photo_id', 'Photo') !!}
+        {!! Form::file('photo_id', null,  ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('password', 'Password') !!}
+        {!! Form::password('password', ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+    </div>
+    {!! Form::close() !!}
+    {{--</form>--}}
+
+    @if(count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+@endsection
