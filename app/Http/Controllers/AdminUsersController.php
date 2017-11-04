22472ea1613104f3f9e52990ffe2e2562b429fb1
arturisav <artur.isaverdyan@gmail.com>
2017-11-04 15:49:55 +0400

    delete functionality

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -122,5 +122,8 @@ class AdminUsersController extends Controller
     public function destroy($id)
     {
         //
+        User::findOrFail($id)->delete;
+        return redirect('/admin/users');
+
     }
 }
